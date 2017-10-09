a866fb74d708c3a4b2f395aa6e66ba91189a641c
fwagner314 <wagneroliveira314@gmail.com>
2017-10-09 18:00:29 -0300

    Login via LDAP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < UserDB
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :ldap_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  before_validation :get_ldap_save
+  
+  def get_ldap_save
+  	self.email = Devise::LDAP::Adapter.get_ldap_param(self.username,"mail").first
+  end
+# hack for remember_token
+def authenticatable_token
+	Digest::SHA1.hexdigest(email)[0,29]
+end
+
 end
