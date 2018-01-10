0f5ff899f5a3e8152bc36c6a49ff435a6af05ef3
fwagner314 <wagneroliveira314@gmail.com>
2018-01-10 14:52:25 -0300

    remoção do seeds do gitignore

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+MetasTaxa.create([{ano: 2015, valor: 61.6}, {ano: 2016, valor: 57.7}, {ano: 2017, valor: 54}, {ano: 2018, valor: 49.5}, {ano: 2019, valor: 44.9}, {ano: 2020, valor: 39.5}])
