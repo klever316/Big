2e29209c348c43d811cfa5e6d70c7405e98e4547
klever316 <fk.a316@gmail.com>
2018-02-26 16:08:51 -0300

    Adicionando pundit ao crime organizado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201162728) do
+ActiveRecord::Schema.define(version: 20180226183713) do
 
   create_table "dia_utils", force: :cascade do |t|
     t.integer "ano"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180201162728) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username"
